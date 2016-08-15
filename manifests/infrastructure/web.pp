353435f289a7b321a8230f1b902d83abc4496fc4
Carl Caum <carl@carlcaum.com>
2016-08-15 15:33:48 -0700

    Manage vsphere VMs as infrastructure component

--- manifests/infrastructure/web.pp
+++ manifests/infrastructure/web.pp
@@ -1,9 +1,16 @@
-define rgbank::infrastructure::web {
+define rgbank::infrastructure::web() {
   vsphere_vm { "/opdx1/vm/carl/${name}":
-    esnure   => present,
-    memory   => 1024,
-    cpus     => 1,
-    source   => '/opdx1/vm/operations/templates/cento-7-x86_64',
+    ensure         => present,
+    memory         => 1024,
+    cpus           => 1,
+    source         => '/opdx1/vm/operations/templates/cento-7-x86_64',
+    source_type    => 'template',
+    create_command => {
+      command   => '/bin/curl',
+      arguments => '-k https://10.32.161.83/packages/current/install.bash | sudo bash',
+      user      => 'root',
+      password  => 'puppetlabs',
+    }
   }
 }
 
