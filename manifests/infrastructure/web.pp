89371742ddeaf88c983d45f32f9f544c109039df
Carl Caum <carl@carlcaum.com>
2016-08-17 10:16:14 -0700

    Wait for network to be available before calling puppet provisioning script

--- manifests/infrastructure/web.pp
+++ manifests/infrastructure/web.pp
@@ -1,17 +1,20 @@
 define rgbank::infrastructure::web(
   $memory = 1024,
   $cpus   = 1,
-  $template = '/opdx1/vm/carl/test',
+  $template = '/opdx1/vm/carl/centos-7-x86_64-puppet',
+  $resource_pool = '/general1',
 ) {
+
   vsphere_vm { "/opdx1/vm/carl/${title}":
     ensure         => present,
     memory         => $memory,
     cpus           => $cpus,
     source         => $template,
     source_type    => 'template',
+    resource_pool  => $resource_pool,
     create_command => {
-      command   => '/bin/curl',
-      arguments => "-k https://10.32.161.83/packages/current/install.bash | sudo bash -s agent:certname=${name}",
+      command   => '/bin/sleep',
+      arguments => "30; /bin/curl -k https://10.32.161.83:8140/packages/puppet_provision.bash | bash -l -s appserver ${title} Sup3rs3cr3t",
       user      => 'root',
       password  => 'puppetlabs',
     }
