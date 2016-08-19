139cb47f2fc17370f1cd929f8053322c33c56875
Carl Caum <carl@carlcaum.com>
2016-08-18 19:53:22 -0700

    Lets try using the balancer name

--- manifests/load.pp
+++ manifests/load.pp
@@ -17,7 +17,7 @@ define rgbank::load (
 
   $balancermembers.each |$member| {
 
-    haproxy::balancermember { "${member['host']}-${name}":
+    haproxy::balancermember { "${member['name']}-${name}":
       listening_service => "rgbank-${name}",
       server_names      => $member['host'],
       ipaddresses       => $member['ip'],
