9bd5ea55b551aaa0cffc48cfd3de213d9add3755
ipcrm <mcadorette@gmail.com>
2016-10-06 19:08:26 -0400

    Correct Ordering Issues/Beaker Hiera Change
    Prior to this commit, if you deployed via archive (instead of git
    checkout), we would not properly deploy with SELinux.  Also, added
    BEAKER_custom_hiera env variable to allow Beaker to use hiera-http
    with Jenkins plugin for testing new releases.

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -17,6 +17,8 @@ RSpec.configure do |c|
     puppet_module_install(:source => proj_root, :module_name => 'rgbank')
     hosts.each do |host|
       on host, puppet('config', 'set', 'app_management', 'true'), { :acceptable_exit_codes => [0,1] }
+      on host, puppet('config', 'set', 'environment', 'beaker'), { :acceptable_exit_codes => [0,1] }
+      shell 'mkdir /etc/puppetlabs/code/environments/beaker', :acceptable_exit_codes => [0]
       on host, puppet('module', 'install', 'puppetlabs-stdlib'), { :acceptable_exit_codes => [0,1] }
       on host, puppet('module', 'install', 'hunner-wordpress'), { :acceptable_exit_codes => [0,1] }
       on host, puppet('module', 'install', 'jfryman-selinux'), { :acceptable_exit_codes => [0,1] }
@@ -26,7 +28,15 @@ RSpec.configure do |c|
       on host, puppet('module', 'install', 'puppetlabs-haproxy'), { :acceptable_exit_codes => [0,1] }
       on host, puppet('module', 'install', 'puppetlabs-mysql'), { :acceptable_exit_codes => [0,1] }
       on host, puppet('module', 'install', 'puppetlabs-vcsrepo'), { :acceptable_exit_codes => [0,1] }
-      on host, puppet('module', 'install', 'gareth-docker'), { :acceptable_exit_codes => [0,1] }
+      on host, puppet('module', 'install', 'camptocamp-archive'), { :acceptable_exit_codes => [0,1] }
+      on host, puppet('module', 'install', 'garethr-docker'), { :acceptable_exit_codes => [0,1] }
+
+      if ENV['BEAKER_custom_hiera'] == 'true'
+        on host, puppet('module', 'install', 'crayfishx-hiera_http'), { :acceptable_exit_codes => [0,1] }
+        shell '/opt/puppetlabs/puppet/bin/gem install hiera-http', :acceptable_exit_codes => [0]
+        scp_to(host, 'hiera.yaml', '/etc/puppetlabs/puppet/hiera.yaml')
+      end
+
     end
   end
 end
