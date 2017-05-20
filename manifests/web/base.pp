5c57032d69796019fdf7d0432f21860ad7a70b51
Carl Caum <carl@carlcaum.com>
2017-05-20 14:43:15 -0700

    Extract to version directory

--- manifests/web/base.pp
+++ manifests/web/base.pp
@@ -76,6 +76,7 @@ define rgbank::web::base(
       archive::artifactory { "rgbank-build-${version}.tar.gz":
         ensure       => present,
         extract      => true,
+        extract_path => "${install_dir_real}/artifactory/rgbank-${version}",
         url          => "http://${artifactory_server}/artifactory/rgbank-web/rgbank-build-${version}.tar.gz",
         archive_path => "${install_dir_real}/artifactory/rgbank-${version}",
         require      => File["${install_dir_real}/artifactory/rgbank-${version}"],
