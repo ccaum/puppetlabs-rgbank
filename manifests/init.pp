365eccb666e5b31886dbd0b9377aa09fbe87dcf6
Carl Caum <carl@carlcaum.com>
2017-08-06 23:41:27 -0700

    Only make components if defined

--- manifests/init.pp
+++ manifests/init.pp
@@ -10,24 +10,28 @@ application rgbank (
   $web_components = collect_component_titles($nodes, Rgbank::Web)
   $load_component = collect_component_titles($nodes, Rgbank::Load)[0] #Assume we only have one
 
-  rgbank::db { $db_component:
-    user     => $db_username,
-    password => $db_password,
-    export   => Database[$db_component],
+  if $db_component {
+    rgbank::db { $db_component:
+      user     => $db_username,
+      password => $db_password,
+      export   => Database[$db_component],
+    }
   }
 
-  $web_https = $web_components.map |$comp_name| {
-    $http = Http["rgbank-web-${comp_name}"]
+  if ($web_components.length() > 0) {
+    $web_https = $web_components.map |$comp_name| {
+      $http = Http["rgbank-web-${comp_name}"]
 
-    rgbank::web { $comp_name:
-      use_docker  => $use_docker,
-      listen_port => String($listen_port),
-      consume     => Database[$db_component],
-      export      => $http,
+      rgbank::web { $comp_name:
+        use_docker  => $use_docker,
+        listen_port => String($listen_port),
+        consume     => Database[$db_component],
+        export      => $http,
+      }
+
+      #Return HTTP service resource
+      $http
     }
-
-    #Return HTTP service resource
-    $http
   }
 
   if $load_component {
