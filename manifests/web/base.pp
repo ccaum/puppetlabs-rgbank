5a81f29daaab27d6330e95637a9f21d3efad3e6a
Carl Caum <carl@carlcaum.com>
2020-03-26 17:55:13 -0700

    Fix syntax error

--- manifests/web/base.pp
+++ manifests/web/base.pp
@@ -57,7 +57,7 @@ define rgbank::web::base(
       wp_content_owner     => nginx,
       wp_content_group     => nginx,
       wp_content_recurse   => true,
-      wp_site_url          => "${::ec2_metadata['public-ipv4']":${listen_port}",
+      wp_site_url          => "${::ec2_metadata['public-ipv4']}":${listen_port}",
     }
 
     case $source_type {
