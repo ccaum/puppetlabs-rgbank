2e8cbb8df4880046f3854251beacf4c1fc502a0a
Carl Caum <carl@carlcaum.com>
2020-04-23 22:28:07 -0700

    Improve verbosity of co task

--- tasks/co.sh
+++ tasks/co.sh
@@ -3,5 +3,7 @@
 cd /opt/rgbank-rgbank/wp-content/themes/rgbank
 git fetch --all
 git co $PT_tag
+echo "Deployed version ${PT_tag} to disk"
 
 systemctl restart nginx
+echo "Successfully deployed version ${PT_tag}"
